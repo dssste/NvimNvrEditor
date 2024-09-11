9797c6ae1e158cbb6815a85e6e2bf01a46ea2188
cz <cyz36@pm.me>
2024-09-11 16:36:57 +0800

    use kitty on linux

--- Assets/NvimNvr/Editor/TermDispatch.cs
+++ Assets/NvimNvr/Editor/TermDispatch.cs
@@ -1,9 +1,20 @@
-using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.IO;
+
+#if (UNITY_EDITOR_WIN)
+
+using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Unity.CodeEditor;
+
+#elif (UNITY_EDITOR_LINUX)
+
+using System.Diagnostics;
+using Unity.CodeEditor;
+
+#endif
 
 public class TermDispatch{
 		private const string nvim_address = "127.0.0.1";
@@ -14,17 +25,21 @@ public class TermDispatch{
 			.GetActiveTcpConnections()
 			.Any(tcpi => tcpi.LocalEndPoint.Port == nvim_port && tcpi.LocalEndPoint.Address.ToString() == nvim_address);
 
+		public static bool Open(string projectPath, string filePath, int line, int column){
+			projectPath = Path.Join(projectPath, "Assets");
+			return OpenByPlatform(projectPath, filePath, line, column);
+		}
 
 #if(UNITY_EDITOR_WIN)
 
 
 		private const string script_editor_process_name = "WindowsTerminal";
 		private static readonly string nvr_argument = $"-s --servername {nvim_address}:{nvim_port} --nostart $(File) +$(Line)";
-		private static readonly string script_editor_start_argument = $"start wt -w 0 nt nvim $(File) +$(Line) -c \"Proj {{0}}\" --listen {nvim_address}:{nvim_port}";
+		private static readonly string script_editor_start_argument = $"start wt -w 0 nt nvim $(File) +$(Line) -c \"cd {{0}}\" --listen {nvim_address}:{nvim_port}";
 		[DllImport("user32.dll")]
 		private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-		public static bool Open(string projectPath, string filePath, int line, int column){
+		private static bool OpenByPlatform(string projectPath, string filePath, int line, int column){
 			if(isNvimRunning){
 				Process
 					.Start(new ProcessStartInfo{
@@ -58,15 +73,27 @@ public class TermDispatch{
 #elif(UNITY_EDITOR_LINUX)
 
 
-		private const string script_editor_process_name = "WindowsTerminal";
 		private static readonly string nvr_argument = $"-s --servername {nvim_address}:{nvim_port} --nostart $(File) +$(Line)";
-		private static readonly string script_editor_start_argument = $"start wt -w 0 nt nvim $(File) +$(Line) -c \"Proj {{0}}\" --listen {nvim_address}:{nvim_port}";
+		private static readonly string script_editor_start_argument = $"nvim $(File) +$(Line) -c \"cd {{0}}\" --listen {nvim_address}:{nvim_port}";
 
-		public static bool Open(string projectPath, string filePath, int line, int column){
+		private static bool OpenByPlatform(string projectPath, string filePath, int line, int column){
 			if(isNvimRunning){
-				UnityEngine.Debug.Log("Nvim is running");
+				var arg = CodeEditor.ParseArgument(nvr_argument, filePath, line, column);
+				Process.Start(new ProcessStartInfo{
+					FileName = "nvr",
+					Arguments = CodeEditor.ParseArgument(nvr_argument, filePath, line, column),
+					CreateNoWindow = true,
+					UseShellExecute = false,
+				});
 			}else{
-				UnityEngine.Debug.Log("Nvim is not running");
+				var arg = string.Format(script_editor_start_argument, projectPath);
+				arg = CodeEditor.ParseArgument(arg, filePath, line, column);
+				Process.Start(new ProcessStartInfo{
+					FileName = "kitty",
+					Arguments = arg,
+					CreateNoWindow = true,
+					UseShellExecute = false,
+				});
 			}
 			return true;
 		}
