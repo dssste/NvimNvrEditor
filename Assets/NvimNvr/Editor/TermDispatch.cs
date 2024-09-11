b79e815d505df6ff2580ad46ea21c02501c99e30
cz <cyz36@pm.me>
2024-09-11 11:39:12 +0800

    leave linux implementations

--- Assets/NvimNvr/Editor/TermDispatch.cs
+++ Assets/NvimNvr/Editor/TermDispatch.cs
@@ -8,18 +8,23 @@ using Unity.CodeEditor;
 public class TermDispatch{
 		private const string nvim_address = "127.0.0.1";
 		private const int nvim_port = 25884;
+
+		private static bool isNvimRunning => IPGlobalProperties
+			.GetIPGlobalProperties()
+			.GetActiveTcpConnections()
+			.Any(tcpi => tcpi.LocalEndPoint.Port == nvim_port && tcpi.LocalEndPoint.Address.ToString() == nvim_address);
+
+
+#if(UNITY_EDITOR_WIN)
+
+
 		private const string script_editor_process_name = "WindowsTerminal";
 		private static readonly string nvr_argument = $"-s --servername {nvim_address}:{nvim_port} --nostart $(File) +$(Line)";
 		private static readonly string script_editor_start_argument = $"start wt -w 0 nt nvim $(File) +$(Line) -c \"Proj {{0}}\" --listen {nvim_address}:{nvim_port}";
-
 		[DllImport("user32.dll")]
 		private static extern bool SetForegroundWindow(IntPtr hWnd);
 
 		public static bool Open(string projectPath, string filePath, int line, int column){
-			var isNvimRunning = IPGlobalProperties
-				.GetIPGlobalProperties()
-				.GetActiveTcpConnections()
-				.Any(tcpi => tcpi.LocalEndPoint.Port == nvim_port && tcpi.LocalEndPoint.Address.ToString() == nvim_address);
 			if(isNvimRunning){
 				Process
 					.Start(new ProcessStartInfo{
@@ -48,4 +53,24 @@ public class TermDispatch{
 			}
 			return true;
 		}
+
+
+#elif(UNITY_EDITOR_LINUX)
+
+
+		private const string script_editor_process_name = "WindowsTerminal";
+		private static readonly string nvr_argument = $"-s --servername {nvim_address}:{nvim_port} --nostart $(File) +$(Line)";
+		private static readonly string script_editor_start_argument = $"start wt -w 0 nt nvim $(File) +$(Line) -c \"Proj {{0}}\" --listen {nvim_address}:{nvim_port}";
+
+		public static bool Open(string projectPath, string filePath, int line, int column){
+			if(isNvimRunning){
+				UnityEngine.Debug.Log("Nvim is running");
+			}else{
+				UnityEngine.Debug.Log("Nvim is not running");
+			}
+			return true;
+		}
+
+
+#endif
 }
